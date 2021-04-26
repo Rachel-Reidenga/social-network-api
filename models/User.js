65bcbca5e376e4fa93b7705af12a6b3e602cef96
Rachel-Reidenga <reidenga@gmail.com>
2021-04-25 22:38:40 -0500

    .json .js

--- models/User.js
+++ models/User.js
@@ -27,6 +27,6 @@ userSchema.virtual('friendCount').get(function() {
     return this.friends.length;
 });
 
-const User = model('User', userSchema);
+const User = model('User', userSchema)
 
-module.exports = User;
+module.exports = User
